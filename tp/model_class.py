0f44dd7cb413e5d58eec4fb3ac2d24b4986b11bc
Olga.Lavrichenko <olga.lavrichenko@jetbrains.com>
2021-12-07 21:12:41 +0300

    Add model learning

--- tp/model_class.py
+++ tp/model_class.py
@@ -4,9 +4,10 @@ import pickle
 
 class ModelCTM:
     def __init__(self, topic_model, size, n_tp, epochs):
-        self.model = ZeroShotTM(
-            bow_size=len(topic_model.vocab), contextual_size=size, n_components=n_tp, num_epochs=epochs
-        )
+        if not topic_model == None:
+            self.model = ZeroShotTM(
+                bow_size=len(topic_model.vocab), contextual_size=size, n_components=n_tp, num_epochs=epochs
+            )
 
     def fit(self, train_data):
         self.model.fit(train_data)
@@ -17,7 +18,7 @@ class ModelCTM:
 
     def load_model(self, path):
         with open(path, "rb") as f:
-            return pickle.load(f)
+            self.model = pickle.load(f)
 
     def topic(self):
         return self.model.get_topics()
